d4a3533988569bdb05d0309cf16f1156b58bfee9
sanickg <elmejorcarnal6@gmail.com>
2025-09-04 23:20:02 -0600

    Añado todos los ejemplos de las notas

---
+++ practicas/p04/inclusion_archivos/pie.inc.php
@@ -0,0 +1,10 @@
+<hr/>
+<?php
+echo "<div><h1 style=\"border-width:3;border-style:groove; background-color:
+#ffcc99;\"> Final de la página PHP Vínculos útiles : <a href=\"http://php.net\">php.net</a>
+&nbsp; <a href=\"http://mysql.org\">mysql.org</a></h1>";
+echo "Nombre del archivo ejecutado: ", $_SERVER['PHP_SELF'],"&nbsp;&nbsp; &nbsp;";
+echo "Nombre del archivo incluido: ", __FILE__ ,"</div>";
+?>
+</body>
+</html>
